a499e8df60be452b75ac0ef00ce0925ff1fcbeb2
นรภัทร พลอยบุญ <dawnrun0147@example.com>
2021-11-12 11:32:31 +0700

    initial commit

---
+++ php202.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf8">
+        <meta name="viewport" content="device=width-device, intinal-scale=1.0">
+        <title>PHP: Hypertext Preprocessor.</title>
+    </head>
+    <body>
+        <h1>Welcome to php</h1>
+
+        
+            <?php 
+                echo "<h2>Hello world!</h2>";
+                echo "<br />";
+                echo "<My Name is Mark sakberg";
+                echo "<br />";
+                print("I Love Facebook");
+            ?>
+        
+    </body>
+</html>
